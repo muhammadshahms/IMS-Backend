0acb50252e3d3021ec1d1dc6ded9f38518172063
muhammadshahms <muhammadshah516@gmail.com>
2025-12-04 12:12:07 +0500

    css

--- User/src/lib/constant.ts
+++
@@ -1,8 +0,0 @@
-type APPNAME = String
-
-export const APPNAME = "Incubation Management System"
-export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || (
-    window.location.hostname === "localhost"
-        ? "http://localhost:3000"
-        : "https://ims-server-sage.vercel.app"
-)
